b026b9af1621355f42172d43744aeef2ecae5819
Nilesh-Yadav11 <ny172005@gmail.com>
2026-06-05 14:41:09 +0530

    Adding 2nd day work

---
+++ Day2/Add_digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>=10){
+            int total=0;
+
+            while(num>0){
+            total=total+num%10;
+            num=num/10;
+        }
+        num=total;
+        }
+        return num;   
+    }
+};
